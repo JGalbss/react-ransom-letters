9621eddb29413ea51b0948d6ea51969042b4458d
Josh Galbreath <joshsgalbreath@gmail.com>
2023-02-07 23:26:42 -0500

    init project

---
+++ react-ransom-letters/example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-ransom-letters'
+import 'react-ransom-letters/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
